bef0ce924f6a92f6733ab8f1aab5fe57cc3eaa01
sezarfen <furkandndr2014@gmail.com>
2024-03-29 17:53:03 +0300

    Bismillahirrahmanirrahim, more progress on flask project, form and validations

--- corey_schafer_tutorials/flaskblog.py
+++ corey_schafer_tutorials/flaskblog.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask import flash, redirect
 from flask import render_template #to render templates instead of single strings
+from flask import url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '1b5a0d4efabb1627bc3d80614699f00f815aa5b7153704bfbf'
 
 posts = [
 	{
@@ -26,6 +31,21 @@ def	getHome():
 @app.route("/about")
 def	getAbout():
 	return render_template("about.html", Title = "About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def getRegister():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		flash(f"Account generated for {form.username.data}!", "success")
+		return redirect(url_for("getHome"))
+	return render_template("register.html", title="Register", form=form)
+
+@app.route("/login")
+def getLogin():
+	form = LoginForm()
+	return render_template("login.html", title="Login", form=form)
+
+
 
 # this part works if we call the app with python3 'filename' | instead of 'flask run'
 if __name__ == '__main__':
