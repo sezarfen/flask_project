3d1a88e6025283f26112cf5dc447f9a49ea2a6d9
sezarfen <furkandndr2014@gmail.com>
2024-04-06 06:16:28 +0300

    Bismillahirrahmanirrahim, starting user authentication InsaAllah

--- flask_and_docs/main.py
+++ flask_and_docs/main.py
@@ -2,6 +2,18 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def get_index():
+	return render_template("index.html")
+
+@app.route("/register", methods = ["GET", "POST"])
+def get_register():
+	if request.method == "GET":
+		return render_template("register.html")
+	elif request.method == "POST":
+		for i in request.form:
+			print(i)
+
 @app.route("/login", methods=["GET", "POST"])
 def get_login():
 	if request.method == "GET":
