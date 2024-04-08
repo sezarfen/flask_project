7ce9c412b0350f47c8389d22b27fb444c7d93334
sezarfen <furkandndr2014@gmail.com>
2024-04-09 01:07:31 +0300

    Bismillahirrahmanirrahim, some database progress

--- codemy/main.py
+++ codemy/main.py
@@ -3,20 +3,42 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask import flash # for flash messages on the screen
 from flask_wtf import FlaskForm # We can also do the forms ourselves, but it is easly helps us to build forms
-from wtforms import StringField, SubmitField, FileField # Different Fields we can import
+from wtforms import StringField, SubmitField, EmailField # Different Fields we can import
 from wtforms.validators import DataRequired # If we something pop-up when someone don't fill that area, this one take cares of it
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
 ## For CSRF Token
 app.config['SECRET_KEY'] = "!+wvnadscgth349G6hr8pERTB_hWrtlkt*12-G43rf"
+## Add Database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+## Initialize The Database
+db = SQLAlchemy(app)
+
+## Generate Model
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(55), nullable = False)
+	email = db.Column(db.String(125), nullable = False, unique = True)
+	date_added = db.Column(db.DateTime, default = datetime.utcnow)
+
+	## Generate a String
+	def __repr__(self):
+		return '<Name %r>' % self.name
+	
+## Generate Form for Model
+class UserForm(FlaskForm):
+	name = StringField("Name", validators=[DataRequired()])
+	email = EmailField("Email", validators=[DataRequired()])
+	submit = SubmitField(label = "Submit!")
 
 
 ## Generate Form Class
 class NamerForm(FlaskForm):
 	name = StringField("What's Your Name", validators=[DataRequired()])
 	submit = SubmitField(label = "Submit It!")
-
-
 
 #################################### ROUTES ####################################
 @app.route("/")
@@ -54,3 +76,24 @@ def name():
 		flash("Form Submitted Successfully!") # We don't have to pass as a paramter to render template, flash knows what to do with it
 
 	return render_template("name.html", name = name, form = form)
+
+@app.route("/user/add", methods = ["GET", "POST"])
+def add_user():
+	name = None
+	form = UserForm()
+
+	if form.validate_on_submit():
+		user = Users.query.filter_by(email = form.email.data).first()
+		if user is None:
+			newUser = Users(name=form.name.data, email = form.email.data)
+			db.session.add(newUser)
+			db.session.commit()
+			flash("User Added successfully!")
+		else:
+			flash("User not added, Email adress is already in use!")
+		name = form.name.data
+		form.name.data = ""
+		form.email.data = ""
+		
+	our_users = Users.query.order_by(Users.id)
+	return render_template("add_user.html", form = form, name = name, our_users = our_users)
